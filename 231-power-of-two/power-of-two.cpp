2ccf14837b6f3ed7b17ead70da516722b4c60128
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-02-19 18:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i=0;i<sqrt(n)+1;i++){
-            if(n==pow(2,i))return true;
-        }
+        if(n>0 and (n&(n-1))==0)return true;
         return false;
     }
 };
