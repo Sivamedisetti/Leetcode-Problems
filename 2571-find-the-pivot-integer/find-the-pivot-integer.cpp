95fa2599b0f2e9fbe516a302c1b427f3c6fb05ca
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-03-13 10:59:34 +0530

    Time: 4 ms (35.26%) | Memory: 7.2 MB (73.82%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int ts=(n*(n+1))/2;
+        if(n==1)return 1;
+        if(n<4)return -1;
+        for(int i=n/2;i<n-1;i++){
+            if((i*(i+1))==(ts-i-1)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
