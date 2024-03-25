148310b6835622612dc48221d1f5d34cbef3223a
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-03-25 11:22:15 +0530

    Time: 40 ms (73.65%) | Memory: 37 MB (51.91%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int>ans;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i-1]==nums[i])ans.push_back(nums[i-1]);
+        }
+        return ans;
+    }
+};
