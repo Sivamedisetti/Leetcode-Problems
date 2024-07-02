151bab4db23433607636b4b274e51830ef2b06bf
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-07-02 11:19:06 +0530

    Time: 3 ms (78.74%) | Memory: 14.2 MB (15.24%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>mp2;
+        unordered_map<int,int>mp2;
         for(auto it:nums2){
             mp2[it]++;
         }
         vector<int>ans;
         for(auto it:nums1){
-            if(mp2[it]!=0){
+            if(mp2[it]>0){
                 ans.push_back(it);
                 mp2[it]--;
             }
