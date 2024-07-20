8ec9d879dc71f89df1dcdbaddc37a30fd6aa1247
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-07-20 10:47:40 +0530

    Time: 35 ms (73.82%) | Memory: 36.1 MB (16.25%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -5,16 +5,10 @@ public:
         vector<vector<int>>result(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(rowSum[i]<=colSum[j]){
-                    result[i][j]=rowSum[i];
-                    colSum[j]-=rowSum[i];
-                    rowSum[i]=0;
-                }
-                else{
-                    result[i][j]=colSum[j];
-                    rowSum[i]-=colSum[j];
-                    colSum[j]=0;
-                }
+                int ans=min(rowSum[i],colSum[j]);
+                result[i][j]=ans;
+                rowSum[i]-=ans;
+                colSum[j]-=ans;
             }
         }
         return result;
