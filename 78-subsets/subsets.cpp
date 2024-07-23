091bcbef5a22e928d8efc0eb60df1f90fe2f633a
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-07-23 15:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (66.28%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void pickNpick(int ind,vector<int>ds,vector<int>&nums){
-        if(ind == nums.size() ){
-            ans.push_back(ds);
-            return;
-        }
-        //pick an element from an arr
-        ds.push_back(nums[ind]);
-        pickNpick(ind+1,ds,nums);
-        ds.pop_back();
-        //non pick element from an arr
-        pickNpick(ind+1,ds,nums);
-        
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>ds;
-        pickNpick(0,ds,nums);
-        return ans;
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
     }
 };
