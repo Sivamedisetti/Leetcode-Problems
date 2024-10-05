af152f0634e35656a92d4c321758f0f4c59a07af
Siva Medisetti <96729473+Sivamedisetti@users.noreply.github.com>
2024-10-05 10:44:34 +0530

    Time: 2978 ms (5.44%) | Memory: 61.5 MB (5.04%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
+    int findPartition(vector<int>& arr, int low, int high) {
+        int pivot = arr[low];  // Choosing the first element as the pivot
+        int i = low, j = high;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i < high) {
+                i++;
+            }
+            while (arr[j] > pivot && j > low) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[low], arr[j]);
+
+        return j;  // Return the partition index
+    }
+
+    void quickSort(vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int partition = findPartition(arr, low, high);
+            quickSort(arr, low, partition - 1);  // Left part
+            quickSort(arr, partition + 1, high); // Right part
+        }
+    }
+
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(),skill.end());
+
         int i = 0, j = skill.size()-1;
+        quickSort(skill, 0, j);
 
         int k= accumulate(skill.begin(),skill.end(),0);
-        cout<<k<<endl;
         k = k/(skill.size()/2);
-        cout<<k;
         long long ans=0;
         while(i<j)
         {
